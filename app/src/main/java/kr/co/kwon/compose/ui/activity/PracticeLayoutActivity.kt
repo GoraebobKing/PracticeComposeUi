c368c1bc47743bcfca37f7ce170b9de7e629f8dc
권혁 <kwon8999@supermvoe.co.kr>
2022-07-07 14:15:35 +0900

    [권 혁] ConstraintLayout 맛보기

--- app/src/main/java/kr/co/kwon/compose/ui/activity/PracticeLayoutActivity.kt
+++ app/src/main/java/kr/co/kwon/compose/ui/activity/PracticeLayoutActivity.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.indication
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,9 +20,14 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.atLeast
+import androidx.constraintlayout.compose.atMost
 import kr.co.kwon.compose.R
 
 /**
@@ -31,7 +38,8 @@ class PracticeLayoutActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            deepLayout()
+//            deepLayout()
+            basicLayout()
         }
     }
 }
@@ -39,7 +47,8 @@ class PracticeLayoutActivity : ComponentActivity() {
 @Preview
 @Composable
 fun DefaultPreview() {
-    deepLayout()
+//    deepLayout()
+    basicLayout()
 }
 
 /**
@@ -67,6 +76,42 @@ fun basicLayout(){
             Text(text = "Box is Worst", fontSize = 20.sp)
         }
 
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxWidth()
+//                .padding(start = 18.dp, end = 18.dp)
+                .background(color = colorResource(id = R.color.color_ff0000))
+        ) {
+
+            val (
+                firstTxt, secondTxt
+            ) = createRefs()
+
+
+            Text(
+                modifier = Modifier.constrainAs(firstTxt) {
+                    top.linkTo(parent.top, margin = 18.dp)
+                    start.linkTo(parent.start, margin = 18.dp)
+                },
+                text = "ConstraintLayout is Best", fontSize = 20.sp)
+
+
+            //width -> Dimension.preferredWrapContent 으로 값을 줄경우 이전 0dp를 주는것과 같음
+            //Dimension.preferredWrapContent  .atMost()  or  .atLeast() 해보지는 않았지만 최대값 또는 최소값일듯?
+            Text(
+                modifier = Modifier.constrainAs(secondTxt) {
+                    top.linkTo(firstTxt.top)
+                    bottom.linkTo(firstTxt.bottom)
+                    start.linkTo(firstTxt.end, margin = 10.dp)
+                    end.linkTo(parent.end, margin = 18.dp)
+                    width = Dimension.preferredWrapContent
+                },
+                text = "ConstraintLayout is Worst", maxLines = 1,
+                fontSize = 20.sp, overflow = TextOverflow.Ellipsis)
+        }
+
+
+
 //        BoxWithConstraints(contentAlignment = ) {
 //
 //        }
@@ -83,12 +128,13 @@ fun deepLayout(){
                 .fillMaxWidth()
                 .height(238.dp)
                 .align(alignment = Alignment.BottomCenter)
-                .background(
-                    color = colorResource(
-                        id = R.color.color_ff0000
-                    )
-                )
+//                .background(
+//                    color = colorResource(
+//                        id = R.color.color_ff0000
+//                    )
+//                )
         ) {
+            //상단 5번과 프레쉬 영역
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -118,12 +164,31 @@ fun deepLayout(){
                 )
             }
 
+            //2번째 광화문 부분
             Box(
-                modifier = Modifier.padding(top = 14.dp, start = 18.dp, end = 18.dp)
+                modifier = Modifier
+                    .padding(top = 14.dp, start = 18.dp, end = 18.dp)
                     .fillMaxWidth()
             ) {
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(20.dp)
+                        .background(
+                            color = colorResource(id = R.color.color_7f3bd8),
+                            shape = RoundedCornerShape(12.dp)
+                        )
+                        .align(alignment = Alignment.Center)
+                )
+
+                /**
+                 * TextView 백그라운드 처리로 하나의 위젯으로
+                 * UI 형태를 만들고 싶었지만 현재 버그인지 지원이 안되는건지 텍스트가 높이가 있을경우
+                 * 
+                 * **/
                 Text(
-                    text = "광화문연가", color = colorResource(id = R.color.color_1e1d24),
+                    text = "광화문", color = colorResource(id = R.color.color_1e1d24),
                     fontSize = 16.sp, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center,
                     modifier = Modifier
                         .width(124.dp)
@@ -134,18 +199,83 @@ fun deepLayout(){
                         )
                         .border(
                             width = 3.dp,
-                            color = colorResource(id = R.color.color_1e1d24),
+                            color = colorResource(id = R.color.color_7f3bd8),
                             shape = RoundedCornerShape(20.dp)
                         )
                         .align(alignment = Alignment.Center)
                 )
 
-//                .fillMaxWidth()
-//                .background(
-//                    colorResource(id = R.color.color_7f3bd8),
-//                    shape = RoundedCornerShape(12.dp)
-//                )
+                /**
+                 * 이전 Button 개발시 사이즈가 적어서 강제로 키워주는 부분이 있었는데
+                 * 컴포즈에서는 자동으로 넓게 영역을 잡아주는듯하다
+                 * **/
+                TextButton(
+                    onClick = {
+
+                    }) {
+                        Text(
+                            text = "교대",
+                            color = colorResource(id = R.color.color_ffffff),
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.W500
+                        )
+                    }
+
+                TextButton(
+                    modifier = Modifier.align(alignment = Alignment.CenterEnd),
+                    onClick = {
+
+                    }) {
+                    Text(
+                        text = "역삼",
+                        color = colorResource(id = R.color.color_ffffff),
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.W500
+                    )
+                }
             }
+
+            //3번째 출도착 영역
+            //예전같으면 ConstraintLayout을 사용하겠지만, compose에서는 트리 구조가 아니라서 복잡하더라도 쓰지 않도록한다.
+            //안쓰면 너무 힘듬,,,
+            ConstraintLayout(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 18.dp, top = 16.dp, end = 18.dp)
+            ) {
+                Text(text = "인천공항1터미널행", fontWeight = FontWeight.W500, fontSize = 12.sp, color = colorResource(
+                    id = R.color.color_606060
+                ))
+            }
+//            Column(
+//
+//            ) {
+//                Row {
+//                    Column {
+//
+//
+//                        Text(
+//                            modifier = Modifier.padding(top = 9.dp),
+//                            text = "인천공항1터미널행", fontWeight = FontWeight.W500, fontSize = 12.sp, color = colorResource(
+//                            id = R.color.color_606060
+//                        ))
+//                    }
+//
+//                    Column(
+//                        modifier = Modifier.padding(start = 8.dp)
+//                    ) {
+//                        Text(text = "곧 도착", fontWeight = FontWeight.W400, fontSize = 12.sp, color = colorResource(
+//                            id = R.color.color_ff0000
+//                        ))
+//
+//                        Text(
+//                            modifier = Modifier.padding(top = 9.dp),
+//                            text = "9분23초", fontWeight = FontWeight.W400, fontSize = 12.sp, color = colorResource(
+//                            id = R.color.color_ff0000
+//                        ))
+//                    }
+//                }
+//            }
 
         }
     }
